e5cf579f46fb366963219f95ee826448e4e35951
David Soltero <david.soltero@icann.org>
2019-04-09 19:38:53 -0400

    remove keys method

--- manifests/init.pp
+++ manifests/init.pp
@@ -39,7 +39,7 @@ class pcapture (
     default => 'absent',
   }
   $_interfaces = $interfaces ? {
-    undef   => $facts['networking']['interfaces'].keys(),
+    undef   => $facts['networking']['interfaces'],
     default => $interfaces,
   }
   $_directories = [ $data, ]
