b7a8d0de3e1530e3f8d6a738528a6f0142abeadb
Mauricio Vergara <mauricio.vergara@icann.org>
2019-04-23 11:58:23 -0700

    Adding params

--- manifests/filter.pp
+++ manifests/filter.pp
@@ -3,6 +3,10 @@
 class pcapture::filter (
   Stdlib::Absolutepath  $pcapfilter = '/usr/local/bin/pcapture-filter.sh',
   Boolean               $enable     = true,
+  Stdlib::Absolutepath  $srcdir     = '/opt/pcap',
+  Stdlib::Absolutepath  $dstdir     = '/opt/pcap-filtered',
+  String                $regexf     = '*.ignored.pcap.xz',
+  String                $filter     = '(dst host 199.7.83.42 or dst host 2001:500:9f::42) and (icmp or icmp6)'
 ) {
   $ensure = $enable ? {
     true    => 'present',
@@ -18,7 +22,7 @@ class pcapture::filter (
   cron {
     'pcapfilter':
       ensure  => $ensure,
-      command => "/usr/bin/flock -n /var/lock/pcapfilter.lock ${pcapfilter}",
+      command => "/usr/bin/flock -n /var/lock/pcapfilter.lock ${pcapfilter} -s ${srcdir} -d ${dstdir} -r ${regexf} -f ${filter}",
       user    => 'root',
       require => File["${pcapfilter}"];
 }
